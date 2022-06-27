561522e9903e8d573c53286aae1a7e01af575b99
Sachin Doley <56785005+sachin-11-afk@users.noreply.github.com>
2022-06-27 19:04:29 +0530

    Add files via upload

--- CustomersForm.php
+++ CustomersForm.php
@@ -10,7 +10,7 @@
     $TotalCost = $_POST['TotalCost'];
 
 	// Database connection
-	$conn = new mysqli('localhost','root','','PrintShastra');
+	$conn = new mysqli('localhost','root','','customersDb');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
