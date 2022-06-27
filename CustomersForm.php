91fc9d9e50f06c45b40c71e422929956d589307b
Sachin Doley <56785005+sachin-11-afk@users.noreply.github.com>
2022-06-27 15:53:13 +0530

    Add files via upload

---
+++ CustomersForm.php
@@ -0,0 +1,27 @@
+<?php
+	$Name = $_POST['Name'];
+    $number = $_POST['number'];
+	$Address = $_POST['Address'];
+    $Country = $_POST['Country'];
+    $CheckInDate = $_POST['CheckOutDate'];
+    $CheckOutDate = $_POST['CheckOutDate'];
+    $PaymentStatus = $_POST['PaymentStatus'];
+    $PaymentMethod = $_POST['PaymentMethod'];
+    $TotalCost = $_POST['TotalCost'];
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','test');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(Name, number, Address, Country, CheckInDate, CheckOutDate, PaymentStatus, PaymentMethod, TotalCost) 
+        values(?, ?, ?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssi", $Name,$number,$Address,$Country,$CheckInDate,$CheckOutDate,$PaymentStatus,$PaymentMethod,$TotalCost);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
